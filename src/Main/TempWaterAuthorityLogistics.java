657418f04a24f20a7d92e7c806d567adb63fe3c8
averagejoe127 <49156743+averagejoe127@users.noreply.github.com>
2019-04-03 19:33:01 -0400

    Update TempWaterAuthorityLogistics.java

--- src/Main/TempWaterAuthorityLogistics.java
+++ src/Main/TempWaterAuthorityLogistics.java
@@ -1,3 +1,11 @@
+/*
+This is the second deliverable for WaterAuthorityLogistics. When you run the program, 
+a list of flushes will appear that the user can interact with. The user can view specific details
+for a certain flush and alter that information or they can switch to the hydrant table and do the
+same tasks. This deliverable of the project does not contain the map view because of a licensing issue
+that will be fixed in the final deliverable.
+*/
+
 package Main;
 
 import Controller.Controller;
